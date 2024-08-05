7c3d591d8a3e2bbf68fe6ef08d9cbe08beb317a8
Glauber Magalhães <glauber.tecinfo@gmail.com>
2024-08-05 15:13:39 -0300

    add middleware

--- app/main.py
+++ app/main.py
@@ -1,13 +1,38 @@
-# app/main.py
 from fastapi import FastAPI
+
+from app.middlewares.strip_api_prefix import StripApiPrefixMiddleware
 from app.routers import cities
 
-app = FastAPI()
+DESCRIPTION = """
+You can use this API to get the value of various indicators for a number of cities at multiple admin levels.
+"""
+
+app = FastAPI(
+    title="WRI Cities Indicators API",
+    description=DESCRIPTION,
+    summary="An indicators API",
+    version="v0",
+    terms_of_service="TBD",
+    contact={
+        "name": "WRI Cities Data Team",
+        "url": "https://citiesindicators.wri.org/",
+        "email": "citiesdata@wri.org",
+    },
+    license_info={
+        "name": "License TBD",
+        "url": "https://opensource.org/licenses/",
+    },
+)
+
+# Middlewares
+app.add_middleware(StripApiPrefixMiddleware)
+
 
 # Health check
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
 
-# Cities
+# Routers
+## Cities
 app.include_router(cities.router, prefix="/cities")
