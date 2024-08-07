940da715f08a95f2b7c903496ee401944c85dc57
Glauber Magalhães <glauber.tecinfo@gmail.com>
2024-08-07 15:10:28 -0300

    fix column names

--- main.py
+++ main.py
@@ -187,7 +187,7 @@ def get_city_indicators(city_id: str, admin_level: str):
             "geo_name",
             "geo_level",
             "geo_parent_name",
-            "indicator_id",
+            "indicator",
             "value",
             "indicator_version",
         ]
@@ -200,7 +200,7 @@ def get_city_indicators(city_id: str, admin_level: str):
             "geo_parent_name",
             "indicator_version",
         ],
-        columns="indicator_id",
+        columns="indicator",
         values="value",
     )
     city_indicators_df.reset_index(inplace=True)
@@ -234,7 +234,7 @@ def get_city_geometry(city_id: str, admin_level: str):
         f"SELECT geo_id, indicator, value FROM indicators WHERE geo_parent_name = '{city_id}' and geo_level = '{admin_level}' and indicator_version=0"
     )
     city_indicators_df = city_indicators_df.pivot(
-        index="geo_id", columns="indicator_id", values="value"
+        index="geo_id", columns="indicator", values="value"
     )
 
     city_geojson = json.loads(city_geometry_df.to_json())
@@ -267,7 +267,7 @@ def get_city_geometry_with_indicators(city_id: str, admin_level: str):
         f"SELECT geo_id, indicator, value FROM indicators WHERE geo_parent_name = '{city_id}' and geo_level = '{admin_level}' and indicator_version=0"
     )
     city_indicators_df = city_indicators_df.pivot(
-        index="geo_id", columns="indicator_id", values="value"
+        index="geo_id", columns="indicator", values="value"
     )
 
     city_gdf = pd.merge(city_geometry_df, city_indicators_df, on="geo_id")
@@ -473,7 +473,7 @@ def get_indicator(indicator_id: str):
         "geo_name",
         "geo_level",
         "geo_parent_name",
-        "indicator_id",
+        "indicator",
         "value",
         "indicator_version",
     ]
@@ -505,7 +505,7 @@ def get_city_indicator(indicator_id: str, city_id: str):
         "geo_name",
         "geo_level",
         "geo_parent_name",
-        "indicator_id",
+        "indicator",
         "value",
         "indicator_version",
     ]
