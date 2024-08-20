8dd80a9c87d1a01f80f7c603cd36c89defa430a7
Glauber Magalhães <glauber.tecinfo@gmail.com>
2024-08-20 16:56:56 -0300

    fix lint

--- app/services/indicators_service.py
+++ app/services/indicators_service.py
@@ -12,7 +12,10 @@ from app.const import (
 )
 from app.repositories.datasets_repository import fetch_datasets
 from app.repositories.projects_repository import fetch_projects
-from app.repositories.indicators_repository import fetch_indicators, fetch_first_indicator
+from app.repositories.indicators_repository import (
+    fetch_indicators,
+    fetch_first_indicator,
+)
 from app.utils.filters import generate_search_query
 from app.utils.settings import Settings
 
