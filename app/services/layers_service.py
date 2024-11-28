38c8897fba31fd14478da5950851ec3f2386720d
arsh <arsh3doda@gmail.com>
2024-11-28 19:21:33 +0530

    CCE-282 Updated Cities CIF Class Name

--- app/services/layers_service.py
+++ app/services/layers_service.py
@@ -61,12 +61,12 @@ def get_city_layer(city_id: str, layer_id: str):
     except json.JSONDecodeError:
         map_styling = {}
         legend_styling = {}
-
+    print(layer_fields)
     return {
         "city_id": city_id,
         "layer_id": layer_id,
         "layer_url": layer_url,
-        "class_name": layer_fields["cif_class_name"],
+        "class_name": layer_fields.get("cif_class_name"),
         "file_type": layer_fields["file_type"],
         "map_styling": map_styling,
         "legend_styling": legend_styling,
