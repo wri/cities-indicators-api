87eb35bfdfd9aebfb41437c44c709053fdcf486d
arsh <arsh3doda@gmail.com>
2025-05-01 11:34:53 +0530

    Hotfix - bug fix in the cleanup_spaces_in_response

--- app/utils/utilities.py
+++ app/utils/utilities.py
@@ -1,18 +1,23 @@
 import copy
 
 
-def cleanup_spaces_in_response(response_dict: dict) -> dict:
-    response_dict_copy = copy.deepcopy(response_dict)
-    for k, v in response_dict_copy.items():
+def cleanup_spaces_in_response(response: dict | list) -> dict | list:
+
+    response_copy = copy.deepcopy(response)
+    if isinstance(response, list):
+        response_copy = enumerate(response_copy)
+    elif isinstance(response, dict):
+        response_copy = response.items()
+    for k, v in response_copy:
         if isinstance(v, str):
-            response_dict[k] = v.strip()
+            response[k] = v.strip()
         elif isinstance(v, list):
             for idx, list_val in enumerate(v):
                 if isinstance(list_val, str):
-                    response_dict[k][idx] = list_val.strip()
+                    response[k][idx] = list_val.strip()
         elif isinstance(v, dict):
             vcopy = copy.deepcopy(v)
             for subk, subv in vcopy.items():
                 if isinstance(subv, str):
-                    response_dict[k][subk] = subv.strip()
-    return response_dict
+                    response[k][subk] = subv.strip()
+    return response
